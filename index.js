e3aafdd9605818819ebd56a37dc20cfa5b398f76
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-06-14 13:04:31 +0300

    update

--- index.js
+++ index.js
@@ -59,11 +59,26 @@ bot.on('message', async (msg) => {
         //await authentication();
         await bot.sendMessage(chatId, 'Новый заказ', {
             reply_markup: {
-                inline_keyboard: [
+                keyboard: [
                     [{text: 'Добавить заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         });
+    }
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.addedItems);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.totalPrice);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
     }
 });
 
